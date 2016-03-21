31edc937344f29c0b3070d13ae50c82e3224751a
themegosh <mathewdf@gmail.com>
2016-03-20 20:45:33 -0400

    Initial commit for MySQL Testing

---
+++ App_Code/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Card
+{
+	public Card()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
