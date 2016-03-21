7e3e4a80715b0a3e88076435402c026c3f232259
themegosh <mathewdf@gmail.com>
2016-03-20 22:29:28 -0400

    Some commits before fixing the folder structure

---
+++ App_Code/CardFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Parser
+/// </summary>
+public static class CardFactory
+{
+	public static List<Card> BuildCards()
+	{
+        List<Card> cards = new List<Card>();
+
+        JsonTextReader reader = new JsonTextReader(new StringReader(json));
+while (reader.Read())
+{
+    if (reader.Value != null)
+    {
+        Console.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
+    }
+    else
+    {
+        Console.WriteLine("Token: {0}", reader.TokenType);
+    }
+}
+
+        return cards;
+
+	}
+}
